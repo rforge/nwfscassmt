26a8f39e83406a49856c082e3b8bf874e1fbf161
jimthor <jimthor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-15 15:49:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nwfscassmt@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- JimThor/AgeingError/nwfscAgeingError/R/cMx.R
+++
@@ -1,3 +0,0 @@
-
-cMx <-
-function(Input){as.matrix(Input)}
--- JimThor/AgeingError/nwfscAgeingError/R/rMx.R
+++
@@ -1,7 +0,0 @@
-
-rMx <-
-function(Input){
-  if(is.vector(Input)){Output<-t(as.matrix(Input))}  
-  if(!is.vector(Input)){Output<-as.matrix(Input)}
-  Output
-}
