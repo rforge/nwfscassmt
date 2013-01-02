3d3b5aa13a35bf93afb1a8949f0649fc5d71df9a
allanhicks <allanhicks@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-02 23:08:04 +0000

    Updated map functions to plot zeros differently and included examples in man files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nwfscassmt@53 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nwfscMapping/R/.lat.to.km.R
+++
@@ -1,6 +0,0 @@
-.lat.to.km <-
-function(lat){
-# lat in degrees
-    lat.rad <- (lat * pi)/180
-    return(111.14 - 0.56 * cos(2 * lat.rad))
-}
--- pkg/nwfscMapping/R/mapBubbles.R
+++ pkg/nwfscMapping/R/mapBubbles.R
@@ -1,7 +1,7 @@
 mapBubbles <-
 function(dat,depths=NULL,plotMap=T,
-            maxValue=max(dat$Z),maxSize=0.3,fgCircle="black",bgCircle=rgb(0,0,0,0.3),type="surface",
-            zeroSize=0.04,fgZero=gray(0.5),bgZero=gray(0.5),
+            maxValue=max(dat$Z),maxSize=0.3,circle.fg="black",circle.bg=rgb(0,0,0,0.3),type="surface",
+            zeroPch=13,zeroCex=0.8,zeroCol=gray(0.5),
             legendPos="bottomright",legendBreaks=NULL,legendType="vert",legendTitle=NULL,legendCex=0.8,legendTitleCex=legendCex,...) 
 {
 #type=surface is sqrt
@@ -25,12 +25,13 @@ function(dat,depths=NULL,plotMap=T,
     }
     ind0 <- dat$Z<=0
     ind <- dat$Z>0
-    symbols(dat[ind0,"Longitude"],dat[ind0,"Latitude"],squares=rep(1,sum(ind0)),add=T,fg=fgZero,bg=fgZero,inches=zeroSize)
+    points(dat[ind0,"Longitude"],dat[ind0,"Latitude"],pch=zeroPch,cex=zeroCex,col=zeroCol)
+    #symbols(dat[ind0,"Longitude"],dat[ind0,"Latitude"],squares=rep(1,sum(ind0)),add=T,fg=fgZero,bg=fgZero,inches=zeroSize)
     tmp <- dat[ind,]
     event <- as.EventData(data.frame(EID=1:nrow(tmp),X=tmp$Longitude,Y=tmp$Latitude,Z=tmp$Z))
-    .addBubbles.ach(event,type=type,z.max=maxValue,max.size=maxSize,symbol.fg=fgCircle,symbol.bg=bgCircle,symbol.zero=F,
+    .addBubbles.ach(event,type=type,z.max=maxValue,max.size=maxSize,symbol.fg=circle.fg,symbol.bg=circle.bg,symbol.zero=F,
                 legend.pos=legendPos,legend.breaks=legendBreaks,legend.type=legendType,legend.title=legendTitle,legend.cex=legendCex,legend.title.cex=legendTitleCex)
-    #symbols(c(-1000,dat[ind,]$Longitude),c(-1000,dat[ind,]$Latitude),circles=sqrt(c(maxValue,dat[ind,]$Z)),inches=maxSize,add=T,fg=fgCircle,bg=bgCircle)
+    #symbols(c(-1000,dat[ind,]$Longitude),c(-1000,dat[ind,]$Latitude),circles=sqrt(c(maxValue,dat[ind,]$Z)),inches=maxSize,add=T,fg=circle.fg,bg=circle.bg)
     
     invisible()
 }
