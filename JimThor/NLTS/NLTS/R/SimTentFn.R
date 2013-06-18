ad0f5be6e42fed5a06363bb06280bebbb19c7261
jimthor <jimthor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-18 16:35:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nwfscassmt@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- JimThor/NLTS/NLTS/R/SimTentFn.R
+++
@@ -1,8 +0,0 @@
-
-SimTentFn <-
-function(Nobs, S){
-  Y = numeric(Nobs)
-  Y[1] = runif(1)
-  for(i in 2:Nobs) Y[i] = TentFn(Y[i-1], S=S)
-  return(Y)
-}
--- JimThor/NLTS/NLTS/R/TentFn.R
+++
@@ -1,7 +0,0 @@
-
-TentFn <-
-function(X, S=2){
-  if(X < 0.5) Y = S*X
-  if(X >= 0.5) Y = S*(1-X)
-  return(Y)
-}
