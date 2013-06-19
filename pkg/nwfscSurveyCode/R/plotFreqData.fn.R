e9e793c134a98c3d68990085b3bdf93e75e03ab3
allanhicks <allanhicks@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-19 14:43:00 +0000

    Updated plotFreqData.fn.R to work with U.999
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nwfscassmt@102 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nwfscSurveyCode/R/plotFreqData.fn.R
+++ pkg/nwfscSurveyCode/R/plotFreqData.fn.R
@@ -1,5 +1,4 @@
-plotFreqData.fn <-
-function(dat,inch=0.15,ylab="Bins",xlab="Year",zero2NAs=T,...) {
+plotFreqData.fn <- function(dat,inch=0.15,ylab="Bins",xlab="Year",zero2NAs=T,main=NULL,xlim=NULL,...) {
     #This function plots frequency data as bubble plots
     #You may want to change all zeros to NA's so that those observations are not plotted.
     #   If you don't then set zero2NAs=F
@@ -8,7 +7,7 @@ function(dat,inch=0.15,ylab="Bins",xlab="Year",zero2NAs=T,...) {
     dat <- dat[,-c(1:6)]
     if(gender==0) {
         #dat <- dat[,1:(ncol(dat)/2)]
-        dat <- dat[,-match("U.999.1",names(dat))]
+        #dat <- dat[,-match("U.999.1",names(dat))]
         #print(names(dat))
         dat <- dat[,-match("U.999",names(dat))]
         #print(names(dat))
@@ -22,15 +21,20 @@ function(dat,inch=0.15,ylab="Bins",xlab="Year",zero2NAs=T,...) {
     y <- as.numeric(substring(names(dat),2))
     y <- y[1:numLens]
     if(zero2NAs) {dat[dat==0] <- NA}
+
+    if(is.null(xlim)) {xlim <- range(x)}
     
     if(gender==0) {
+        if(is.null(main)) {main <- "Unsexed+Males+Females"}
         z <- c(unlist(dat[,1:numLens]),max(dat))
-        symbols(c(rep(x,length(y)),0),c(rep(y,each=length(x)),0),circles=z,main="Unsexed+Males+Females",inches=inch,xlab=xlab,ylab=ylab,xlim=range(x),...)
+        symbols(c(rep(x,length(y)),0),c(rep(y,each=length(x)),0),circles=z,main=main,inches=inch,xlab=xlab,ylab=ylab,xlim=xlim,...)
     }
     if(gender==3) {
+        if(is.null(main)) {main <- "Female"}
         z <- c(unlist(dat[,1:numLens]),max(dat))
-        symbols(c(rep(x,length(y)),0),c(rep(y,each=length(x)),0),circles=z,main="Female",inches=inch,xlab=xlab,ylab=ylab,xlim=range(x),...)
+        symbols(c(rep(x,length(y)),0),c(rep(y,each=length(x)),0),circles=z,main=main,inches=inch,xlab=xlab,ylab=ylab,xlim=xlim,...)
+        if(is.null(main)) {main <- "Male"}
         z <- c(unlist(dat[,(numLens+1):ncol(dat)]),max(dat))
-        symbols(c(rep(x,length(y)),0),c(rep(y,each=length(x)),0),circles=z,main="Male",inches=inch,xlab=xlab,ylab=ylab,xlim=range(x),...)
+        symbols(c(rep(x,length(y)),0),c(rep(y,each=length(x)),0),circles=z,main=main,inches=inch,xlab=xlab,ylab=ylab,xlim=xlim,...)
     }
 }
