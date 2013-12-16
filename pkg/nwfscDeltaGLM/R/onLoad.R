c955966aa6196c84df927a3f7ca4a758ec0f7317
eward <eward@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-16 22:00:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nwfscassmt@169 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nwfscDeltaGLM/R/onLoad.R
+++ pkg/nwfscDeltaGLM/R/onLoad.R
@@ -7,12 +7,11 @@ require(pscl)
 require(statmod)
 require(stats)
 
-runif(1)
-load.module("glm")
+#load.module("glm")
 options(stringsAsFactors=TRUE)
 Letters = apply(MARGIN=1,FUN=paste,collapse="",expand.grid(letters,letters))
 
 # assign to this environment to keep from overwriting user's workspace
 assign("Letters", Letters, envir=.GlobalEnv)
-
+assign("runif1", runif(1), envir=.GlobalEnv)
 }
