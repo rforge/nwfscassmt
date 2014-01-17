661d8d3ef92e1e1f51742392a5d9c9568e96f344
jimthor <jimthor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-17 16:54:45 +0000

    Based on description here (http://cran.r-project.org/doc/manuals/R-exts.html#The-DESCRIPTION-file), I have updated the onLoad to eliminate all "require()" statements, and instead put them in the DESCRIPTION file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nwfscassmt@202 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nwfscDeltaGLM/R/onLoad.R
+++ pkg/nwfscDeltaGLM/R/onLoad.R
@@ -3,14 +3,14 @@
 #ToInstall = setdiff( c("rjags","R2jags","runjags","superdiag","pscl","statmod","stats"), installed.packages() )
 #if(length(ToInstall)>0) install.packages( ToInstall )
 
-# load default data into workspace
-require(rjags)
-require(R2jags)
-require(runjags)
-require(superdiag)
-require(pscl)
-require(statmod)
-require(stats)
+# load default data into workspace (THIS IS NOW DONE IN THE DESCRIPTION FILE
+#require(rjags)
+#require(R2jags)
+#require(runjags)
+#require(superdiag)
+#require(pscl)
+#require(statmod)
+#require(stats)
 
 options(stringsAsFactors=TRUE)
 Letters = apply(MARGIN=1,FUN=paste,collapse="",expand.grid(letters,letters))
