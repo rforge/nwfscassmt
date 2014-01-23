9a1cb245a6d145531eddd7bf8cdf56f7076bfbd2
jimthor <jimthor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-23 00:27:25 +0000

    Name and example changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nwfscassmt@237 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nwfscDeltaGLM/examples/Example_delta-GLMM.R
+++ pkg/nwfscDeltaGLM/examples/Example_delta-GLMM.R
@@ -1,24 +1,24 @@
 
 
-my.wd<- SourceFile <- "C:/Users/James.Thorson/Desktop/"
 
 install.packages("nwfscDeltaGLM", repos="http://R-Forge.R-project.org")
 library(nwfscDeltaGLM)
+# updateDeltaGLMcode()
 
 # read in the master data file
-DataFile = SourceFile
-setwd(SourceFile)
+my.wd <- "C:/Users/James.Thorson/Desktop/"
+setwd(my.wd)
 
 # Load data and strata
 data(Example_Species)
 masterDat = Example_Species
 strata.limits <- readIn(ncol=5,nlines=6)
   STRATA  NLat SLat MinDepth MaxDepth
-  A      49.0 36.0  55        183
-  B      49.0 36.0  183       549
-  C      49.0 34.5  549       700
+  First      49.0 36.0  55        183
+  Second      49.0 36.0  183       549
+  Mid      49.0 34.5  549       700
   D      36.0 34.5  55        549
-  E      34.5 32.0  55        549
+  South      34.5 32.0  55        549
 
 # Modify data slightly
 species = "Example_Species"
@@ -30,10 +30,9 @@ processData()
 # Define settings
 mcmc.control = list(chains=2, thin=1, burnin=1e3, iterToSave=1e3)
 Parallel = FALSE   # If having trouble, try turning off parallel
-modelStructure1 = list("StrataYear.positiveTows"="fixed", "VesselYear.positiveTows"="random", "Vessel.positiveTows"="zero", "StrataYear.zeroTows"="fixed", "VesselYear.zeroTows"="random", "Vessel.zeroTows"="zero", "Catchability.positiveTows"="one", "Catchability.zeroTows"="zero", "year.deviations"="fixed", "strata.deviations"="fixed")
+modelStructure1 = list("StrataYear.positiveTows"="fixed", "VesselYear.positiveTows"="randomExpanded", "Vessel.positiveTows"="zero", "StrataYear.zeroTows"="fixed", "VesselYear.zeroTows"="randomExpanded", "Vessel.zeroTows"="zero", "Catchability.positiveTows"="one", "Catchability.zeroTows"="zero", "year.deviations"="fixed", "strata.deviations"="fixed")
 
 # Define models
-attach(Data)
 mods = list()
 mods[[1]] = fitDeltaGLM(modelStructure=modelStructure1, mcmc.control=mcmc.control,Parallel=Parallel, Species=species)
 
