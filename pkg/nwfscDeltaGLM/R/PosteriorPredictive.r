d2c6d91b47890d9f0c6e918b2725b3b0753e87e0
jimthor <jimthor@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-24 05:53:06 +0000

    Added computations for WAIC into PosteriorPredictive.r (so that it can use the correct definition of the likelihood for ECE distributions)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nwfscassmt@243 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nwfscDeltaGLM/R/PosteriorPredictive.r
+++ pkg/nwfscDeltaGLM/R/PosteriorPredictive.r
@@ -3,16 +3,22 @@ PosteriorPredictive = function(Data, Model, maxDims=6, FileName, Folder=NA){
   
   # Attach stuff
   attach(Model$BUGSoutput$sims.list)
-  #attach(Data)
+  on.exit( detach(Model$BUGSoutput$sims.list) )
   Dist = Model$likelihood
   nonZeros = which(Data[,'isNonZeroTrawl']==TRUE)
-  u.nz = matrix(NA, nrow=nrow(B.pos), ncol=length(nonZeros))
+  ll.nz = u.nz = matrix(NA, nrow=nrow(B.pos), ncol=length(nonZeros))  # ll.nz = log-likelihood for non-zero observations
+  ll.z = u.z = matrix(NA, nrow=nrow(B.pos), ncol=nrow(Data)) 
   
   # Warnings about mismatch between data and model
   if(nlevels(strata)!=ncol(cMx(Sdev)) | nlevels(year)!=ncol(Ydev) | nlevels(strataYear)!=ncol(SYdev)| nlevels(vesselYear)!=ncol(VYdev)){
     stop("Model and data do not match")
   }
   
+  # Predictions for Zeros
+  for(i in 1:nrow(Data)){
+    u.z[,i] <- plogis( cMx(pSdev)[,strata[i]] + pYdev[,year[i]] + pVYdev[,vesselYear[i]] + pVdev[,vessel[i]] + pSYdev[,strataYear[i]] + B.zero[,1]*logeffort[i] + B.zero[,2]*logeffort2[i] )
+  }
+  # Predictions for Non-zeros
   if(Dist=="lognormal"){
     sigma = sqrt(log(1+(CV[,1]^2)))
     for(i in 1:length(nonZeros)){
@@ -58,6 +64,12 @@ PosteriorPredictive = function(Data, Model, maxDims=6, FileName, Folder=NA){
     }
   }
   
+  # Zeros: Calculate log-likelihood of the data
+  for(ObsI in 1:nrow(Data)){
+    ll.z[,ObsI] = dbinom( ifelse(Data[ObsI,'HAUL_WT_KG']>0,1,0), size=1, prob=u.z[,ObsI], log=TRUE )
+  }
+  
+  # Non-zeroes: Simulate predictions, and calculate log-likelihood of the data for use in WAIC
   Q = rep(NA, nrow(Data)) # vector to track quantiles for each observation
   Nstrat = length(unique(strataYear[nonZeros]))
   Ncol=ceiling(sqrt(Nstrat)); Nrow = ceiling(Nstrat/Ncol)
@@ -67,43 +79,49 @@ PosteriorPredictive = function(Data, Model, maxDims=6, FileName, Folder=NA){
     ParSet = (PlotI-1)*maxDims^2 + 1:min(Nstrat-(PlotI-1)*maxDims^2,maxDims^2)
     Ncol=ceiling(sqrt(length(ParSet)))
     Nrow = ceiling(length(ParSet)/Ncol)
+    # Make plot while calculating posterior predictives
     jpeg(paste(Folder,"/",FileName,"Posterior_Predictive_",PlotI,".jpg",sep=""),width=Ncol*1.5,height=Nrow*1.5,res=200,units="in")
-    par(mfrow=c(Nrow,Ncol), mar=c(2,2,2,0), mgp=c(1.25,0.25,0), tck=-0.02)
-    for(StrataYearI in ParSet){
-      Which = which(strataYear[nonZeros]==unique(strataYear[nonZeros])[StrataYearI])
-      plot(Data[nonZeros[Which],'HAUL_WT_KG'],ylab="",xlab="",log="y",main=unique(strataYear[nonZeros])[StrataYearI],col="blue", ylim=range(Data[nonZeros,'HAUL_WT_KG']))
-      # mean(u.nz[,2])
-      for(ObsI in 1:length(Which)){
-        if(Dist=="lognormal"){     
-          y = rlnorm(n=1000,meanlog=log(u.nz[,Which[ObsI]]),sdlog=sigma)   # Plotting in log-space
-        }
-        if(Dist=="gamma"){     
-          b = gamma.a / u.nz[,Which[ObsI]];    
-          y = rgamma(n=1000,shape=gamma.a,rate=b)
-        }
-        if(Dist=="invGaussian"){     
-          lambda = u.nz[,Which[ObsI]]*oneOverCV2
-          y = rinvgauss(n=1000,mu=u.nz[,Which[ObsI]],lambda=lambda)
-        }
-        if(Dist=="lognormalECE" | Dist=="lognormalECE2"){     
-          ECE = rbinom(n=nrow(u.nz)*10, size=1, prob=p.ece[,2])
-          y = rlnorm(n=1000, meanlog=log(u.nz[,Which[ObsI]])*(1-ECE)+log(u.nz2[,Which[ObsI]])*ECE, sdlog=sigma*(1-ECE)+sigma2*ECE)
-        }
-        if(Dist=="gammaECE" | Dist=="gammaECE2"){     
-          b = gamma.a / u.nz[,Which[ObsI]];    
-          b2 = gamma.a2 / u.nz2[,Which[ObsI]];    
-          ECE = rbinom(n=nrow(u.nz)*10, size=1, prob=p.ece[,2])
-          y = rgamma(n=1000, shape=gamma.a*(1-ECE)+gamma.a2*ECE, rate=b*(1-ECE)+b2*ECE)
-        }
-        Q[nonZeros[Which[ObsI]]] = mean(y>Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'])
-        Quantiles = quantile(y,prob=c(0.025,0.25,0.75,0.975))
-        lines(x=c(ObsI,ObsI), y=Quantiles[2:3], lwd=2)
-        lines(x=c(ObsI,ObsI), y=Quantiles[c(1,4)], lwd=1,lty="dotted")
-        if(Data[nonZeros[Which[ObsI]],'HAUL_WT_KG']>max(Quantiles) | Data[nonZeros[Which[ObsI]],'HAUL_WT_KG']<min(Quantiles)){
-          points(x=ObsI,y=Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'],pch=4,col="red",cex=2)
+      par(mfrow=c(Nrow,Ncol), mar=c(2,2,2,0), mgp=c(1.25,0.25,0), tck=-0.02)
+      for(StrataYearI in ParSet){
+        Which = which(strataYear[nonZeros]==unique(strataYear[nonZeros])[StrataYearI])
+        plot(Data[nonZeros[Which],'HAUL_WT_KG'],ylab="",xlab="",log="y",main=unique(strataYear[nonZeros])[StrataYearI],col="blue", ylim=range(Data[nonZeros,'HAUL_WT_KG']))
+        # mean(u.nz[,2])
+        for(ObsI in 1:length(Which)){
+          if(Dist=="lognormal"){     
+            y = rlnorm(n=1000,meanlog=log(u.nz[,Which[ObsI]]),sdlog=sigma)   # Plotting in log-space
+            ll.nz[,Which[ObsI]] = dlnorm( Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'],meanlog=log(u.nz[,Which[ObsI]]),sdlog=sigma,log=TRUE)
+          }
+          if(Dist=="gamma"){     
+            b = gamma.a / u.nz[,Which[ObsI]];    
+            y = rgamma(n=1000,shape=gamma.a,rate=b)
+            ll.nz[,Which[ObsI]] = dgamma( Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'],shape=gamma.a,rate=b,log=TRUE)
+          }
+          if(Dist=="invGaussian"){     
+            lambda = u.nz[,Which[ObsI]]*oneOverCV2
+            y = rinvgauss(n=1000,mu=u.nz[,Which[ObsI]],lambda=lambda)
+            ll.nz[,Which[ObsI]] = dinvgauss( Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'],mu=u.nz[,Which[ObsI]],lambda=lambda,log=TRUE)
+          }
+          if(Dist=="lognormalECE" | Dist=="lognormalECE2"){     
+            ECE = rbinom(n=nrow(u.nz)*10, size=1, prob=p.ece[,2])
+            y = rlnorm(n=1000, meanlog=log(u.nz[,Which[ObsI]])*(1-ECE)+log(u.nz2[,Which[ObsI]])*ECE, sdlog=sigma*(1-ECE)+sigma2*ECE)
+            ll.nz[,Which[ObsI]] = dlnorm( Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'], meanlog=log(u.nz[,Which[ObsI]])*(1-ECE)+log(u.nz2[,Which[ObsI]])*ECE, sdlog=sigma*(1-ECE)+sigma2*ECE,log=TRUE)
+          }
+          if(Dist=="gammaECE" | Dist=="gammaECE2"){     
+            b = gamma.a / u.nz[,Which[ObsI]];    
+            b2 = gamma.a2 / u.nz2[,Which[ObsI]];    
+            ECE = rbinom(n=nrow(u.nz)*10, size=1, prob=p.ece[,2])
+            y = rgamma(n=1000, shape=gamma.a*(1-ECE)+gamma.a2*ECE, rate=b*(1-ECE)+b2*ECE)
+            ll.nz[,Which[ObsI]] = dgamma( Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'], shape=gamma.a*(1-ECE)+gamma.a2*ECE, rate=b*(1-ECE)+b2*ECE,log=TRUE)
+          }
+          Q[nonZeros[Which[ObsI]]] = mean(y>Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'])
+          Quantiles = quantile(y,prob=c(0.025,0.25,0.75,0.975))
+          lines(x=c(ObsI,ObsI), y=Quantiles[2:3], lwd=2)
+          lines(x=c(ObsI,ObsI), y=Quantiles[c(1,4)], lwd=1,lty="dotted")
+          if(Data[nonZeros[Which[ObsI]],'HAUL_WT_KG']>max(Quantiles) | Data[nonZeros[Which[ObsI]],'HAUL_WT_KG']<min(Quantiles)){
+            points(x=ObsI,y=Data[nonZeros[Which[ObsI]],'HAUL_WT_KG'],pch=4,col="red",cex=2)
+          }
         }
       }
-    }
     dev.off()
   }
   
@@ -116,7 +134,8 @@ PosteriorPredictive = function(Data, Model, maxDims=6, FileName, Folder=NA){
   abline(a=0,b=1)
   dev.off()
   
-  # Detach stuff
-  #detach(Data)
-  detach(Model$BUGSoutput$sims.list)
+  # Calculate WAIC
+  p_WAIC = 2*sum(log(colMeans(exp(ll.nz))) - colMeans( ll.nz )) + 2*sum(log(colMeans(exp(ll.z))) - colMeans( ll.z ))
+  WAIC = -2*sum(log(colMeans(exp(ll.nz)))) + 2*p_WAIC
+  capture.output( paste("WAIC=",round(WAIC,2)," p_WAIC=",round(p_WAIC,2),sep=""), file=paste(Folder,"/",FileName,"WAIC.txt",sep=""))
 }
